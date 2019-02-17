5a2c0531d1df99f7d4cab471b0898db0807efdec
Ezra Boley <eboley@wisc.edu>
2019-02-16 22:36:02 -0600

    Created some abstractions for our parking spots

---
+++ website/parking.js
@@ -0,0 +1,86 @@
+class ParkingLot {
+    constructor(corners, id) {
+        this.corners = corners;
+        this.spots = [];
+        this.polygon = {};
+        this.id = id;
+    }
+    
+    getCorners() {
+        return this.corners;    
+    }
+    
+    draw() {
+        this.polygon = new google.maps.Polygon({
+            paths: this.corners,
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.6,
+            strokeWeight: 2
+            fillColor: '#FF0000',
+            fillOpacity: 0.4
+        });
+        this.polygon.setMap(map);
+        this.spots.forEach((spot) => {
+            spot.draw();
+        });
+    }  
+    
+    addSpot(corners) {
+        this.spots.push(new ParkingSpot(corners, this, this.createSpotId()));
+    }
+    
+    createSpotId() {
+        return this.id + "_" + this.numSpots
+    }
+
+    howManySpotsFree() {
+        let numFree = 0;
+        this.spots.forEach((spot) => {
+            if (spot.isFree()) 
+                numFree++;
+        });
+        return numFree;
+    }
+}
+
+
+class ParkingSpot {
+    constructor(corners, lot, id) {
+        this.lot = lot;
+        this.corners = corners;
+        this.rectangle;
+        this.isFree = true;
+        this.STATUS_COLOR = {FREE: "green", FULL: "red"};
+        this.id = id;
+    }
+
+    draw() {
+        this.rectangle = new google.maps.Rectangle({
+            paths: corners;
+            strokeColor: "#000000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: this.isFree ? this.STATUS_COLOR.FREE : this.STATUS_COLOR.FULL,
+            fillOpacity: 0.4
+        });
+        this.rectangle.setMap(map);
+    }
+
+    isFree() {
+        return this.isFree;
+    }
+
+    free() {
+        this.rectangle.setOptions({
+            fillColor: this.STATUS_COLOR.FREE
+        });
+        this.isFree = true;
+    }
+
+    occupy() {
+        this.rectangle.setOptions({
+            fillColor: this.STATUS_COLOR.FULL
+        });
+        this.isFree = false;
+    }
+}
