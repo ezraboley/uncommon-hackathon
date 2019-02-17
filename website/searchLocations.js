cac32117cf6a1131cce899bbc800cc4fec1d02d2
Zoei Faith <zoeifaith@Zoeis-MacBook-Air.local>
2019-02-17 05:23:06 -0600

    Remove transition.js and zip state

--- website/searchLocations.js
+++ website/searchLocations.js
@@ -1,3 +1,17 @@
+// Get the input field
+var input = document.getElementById("locate");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("btn").click();
+  }
+}); 
+
 function search() {
     var location = document.getElementById("locate").value;
     var geocoder; //To use later
@@ -7,7 +21,7 @@ function search() {
                     }, function (results, status) {
                             if (status == google.maps.GeocoderStatus.OK) {
                                     map.setCenter(results[0].geometry.location);
-                                map.setZoom(12)
+                                map.setZoom(20)
                                     var marker = new google.maps.Marker({
                                             map: map,
                                             position: results[0].geometry.location
@@ -17,5 +31,7 @@ function search() {
                             }
                     
                     });
+    document.getElementById("googlemaps").style.opacity = "1";;
+    
 }
 
--- website/transitions.js
+++ website/transitions.js
@@ -1,18 +1,12 @@
-const searchBar = document.getElementById("form");
-const mapdiv    = document.getElementById("map");
+const intputField = document.getElementById("locate");
+
+
 
 function zipToAddr() {
-    searchBar.placeholder = "Address";
-    map.setOptions({gestureHandling: "auto"});
-    map.setZoom(15);
+    
 }
 
 function addrToLots() {
-    searchBar.style.position = "absolute";
-    searchBar.style.top = "50px";
-    searchBar.style.left = "50px";
-    searchBar.placeholder = "ZIP Code";
-    mapdiv.style.opacity = "1";    
-    // Highlight the parking places, query
+   
     
 }
