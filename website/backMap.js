9db0ec3faf47c8d099c9de77dc148a5fa3a9180e
Zoei Faith <zoeifaith@Zoeis-MacBook-Air.local>
2019-02-17 04:10:37 -0600

    Cosmetic/functional changes to search pages

---
+++ website/backMap.js
@@ -0,0 +1,20 @@
+var map;
+var google;
+
+var parkingLots = [];
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 41.7997015, lng: -87.5913876}, zoom: 20, mapTypeId: google.maps.MapTypeId.SATELLITE
+        });
+    map.setOptions({disableDefaultUI: "false"});
+
+
+    
+}
+
+
+
+
+
