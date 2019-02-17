8365d1fba981e466d20bc928626b6faf83294d7d
Zoei Faith <zoeifaith@Zoeis-MacBook-Air.local>
2019-02-17 11:56:26 -0600

    added parking lot

--- website/parking.js
+++ website/parking.js
@@ -13,10 +13,9 @@ class ParkingLot {
     draw() {
         this.polygon = new google.maps.Polygon({
             paths: this.corners,
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.6,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
+            fillColor: '#4F4F4F',
+            fillOpacity: .6,
+            strokeWeight: 5,
             fillOpacity: 0.4
         });
         this.polygon.setMap(map);
